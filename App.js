8a7e1137fddfd5ce3d67fac6ccb36a3f6aed245e
Taha Shiad <m.tahashahid@gmail.com>
2021-04-20 17:02:06 +0500

    update lib address because of change in navigation name from navigation to index

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
 import React from "react";
-
 import { Provider } from "react-redux";
 import store from "./app/redux/store";
-import Navigations from "./app/Natigation/Nagivations";
+import Navigations from "./app/Natigation/Index";
 
 const App = () => {
   return (
